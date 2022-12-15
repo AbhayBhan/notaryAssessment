2cd855af355c49e073d91d10b1e537ea9e0037a5
AbhayBhan <abhaybhan197@gmail.com>
2022-12-15 20:15:41 +0530

    indev setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,21 @@ import Details from './components/Details';
 import Form from './components/Form';
 
 function App() {
+
+  const getData = () => {
+    axios.post("https://notaryapp-staging.herokuapp.com/plugin/getPluginSampleResponse",{username : "demo"})
+    .then((res) => {
+      console.log(res);
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
   
   return (
     <div className='container flex flex-row justify-start'>
       <Details />
       <Form />
+      <button onClick={getData}>Click</button>
     </div>
   )
 }
